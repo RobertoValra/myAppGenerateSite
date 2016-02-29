4d74abb01623c1cb228a4d6fa5dbffe49c6ba53d
RobertoValra <robertovalra@hotmail.com>
2016-02-28 21:06:57 -0600

    Added files via upload

--- js/services.js
+++ js/services.js
@@ -13,18 +13,7 @@ angular.module('starter.services', [])
 
   return {
     all: function() {
-      return chats;
+      return food;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
-    }
   };
 });
