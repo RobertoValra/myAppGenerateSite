89db277dbb95e1ea41dfdfabd2f770fecc4f4e30
RobertoValra <robertovalra@hotmail.com>
2016-02-28 18:29:30 -0600

    Update services.js

--- js/services.js
+++ js/services.js
@@ -1,47 +1,47 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
+.factory('productInfo', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var chats = [{
+  var productInfo = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
+    product: 'Felps',
+    price: 'You on your way?',
+    img: 'img/ben.png'
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    product: 'Felps',
+    price: 'You on your way?',
+    img: 'img/ben.png'
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
+    product: 'Felps',
+    price: 'You on your way?',
+    img: 'img/ben.png'
   }, {
     id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
+    product: 'Felps',
+    price: 'You on your way?',
+    img: 'img/ben.png'
   }, {
     id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    product: 'Felps',
+    price: 'You on your way?',
+    img: 'img/ben.png'
   }];
 
   return {
     all: function() {
-      return chats;
+      return productInfo;
     },
     remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
+      productInfo.splice(productInfo.indexOf(chat), 1);
     },
     get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+      for (var i = 0; i < productInfo.length; i++) {
+        if (productInfo[i].id === parseInt(chatId)) {
+          return productInfo[i];
         }
       }
       return null;
